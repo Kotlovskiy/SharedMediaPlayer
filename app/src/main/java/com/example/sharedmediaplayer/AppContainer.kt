43aaed71d6b76a344e6eb60009fefb276ccfca18
Yaroslav Kotlovskiy <ya111ep@mail.ru>
2026-05-25 00:33:04 +0700

    Избавился рот вложенного скаффолда. (оказывается AppTopBar тоже учитывается системные панели, надо разобраться, как это отключить)

--- app/src/main/java/com/example/sharedmediaplayer/AppContainer.kt
+++ app/src/main/java/com/example/sharedmediaplayer/AppContainer.kt
@@ -1,15 +1,14 @@
 package com.example.sharedmediaplayer
 
-import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
-@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AppContainer(
     topBar: @Composable () -> Unit = {},
@@ -17,12 +16,14 @@ fun AppContainer(
     verticalPadding: Dp = 0.dp,
     content: @Composable () -> Unit
 ) {
-    Scaffold(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(horizontal = horizontalPadding, vertical = verticalPadding),
-        topBar = topBar
-    ) {
-        content()
+    Column(modifier = Modifier.fillMaxSize()) {
+        topBar()
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = horizontalPadding, vertical = verticalPadding),
+        ) {
+            content()
+        }
     }
 }
--- app/src/main/java/com/example/sharedmediaplayer/MainActivity.kt
+++ app/src/main/java/com/example/sharedmediaplayer/MainActivity.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
