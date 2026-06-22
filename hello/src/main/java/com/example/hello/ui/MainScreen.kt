ffc4e5b150b43fd29b6121433ae992fdc3bf8dce
Yaroslav Kotlovskiy <ya111ep@mail.ru>
2026-06-22 09:51:18 +0700

    Попытался сделать приветственный экран покрасивее

--- hello/src/main/java/com/example/hello/ui/MainScreen.kt
+++ hello/src/main/java/com/example/hello/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.example.hello.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,13 +9,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import com.example.core_ui.theme.Typography
 import com.example.hello.R
 
 @Composable
@@ -24,35 +27,52 @@ fun HelloScreen(
     onJoinRoom: () -> Unit
 ) {
     Column(
-        modifier.padding(horizontal = 16.dp),
+        modifier = modifier,
         verticalArrangement = Arrangement.Center
     ) {
-        Button(
-            modifier = Modifier.fillMaxWidth().heightIn(min = 60.dp, max = 120.dp),
-            onClick = {
-                onCreateRoom()
-            }
+        Spacer(modifier = Modifier
+            .height(1.dp)
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.primary)
+        )
+        Column(
+            Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+            verticalArrangement = Arrangement.Center
         ) {
-            Text(
-                text = stringResource(R.string.create_room),
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp
-            )
-        }
-
-        Spacer(modifier = Modifier.height(48.dp))
-
-        Button(
-            modifier = Modifier.fillMaxWidth().heightIn(min = 60.dp, max = 120.dp),
-            onClick = {
-                onJoinRoom()
+            Button(
+                modifier = Modifier.fillMaxWidth().heightIn(min = 60.dp, max = 120.dp),
+                onClick = {
+                    onCreateRoom()
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.create_room),
+                    textAlign = TextAlign.Center,
+                    style = Typography.titleLarge
+                )
             }
-        ) {
-            Text(
-                text = stringResource(R.string.join_room),
-                textAlign = TextAlign.Center,
-                fontSize = 24.sp
+
+            Spacer(modifier = Modifier.height(48.dp))
+
+            OutlinedButton(
+                modifier = Modifier.fillMaxWidth().heightIn(min = 60.dp, max = 120.dp),
+                onClick = {
+                    onJoinRoom()
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.join_room),
+                    textAlign = TextAlign.Center,
+                    style = Typography.titleLarge
+                )
+            }
+
+            Spacer(modifier = Modifier
+                .height(1.dp)
+                .fillMaxWidth()
+                .background(color = MaterialTheme.colorScheme.primary)
             )
         }
     }
+
 }
